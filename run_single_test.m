2d59acee36bca4eb4c3393c34d2ae6cd39334969
Kory Mathewson <korymath@gmail.com>
2016-11-05 17:54:07 -0600

    starting revision for new data protocol

--- run_single_test.m
+++ run_single_test.m
@@ -2,8 +2,8 @@
 exp_var.trainCond = 'all';
 exp_var.testCond = 'all';
 exp_var.expStr = 'test';
-exp_var.testStr = 'A12_CalibC_B_01_combined_calNum3.mat';
-exp_var.trainStr = 'A12_CalibC_B_01_combined_calNum3.mat';
+exp_var.testStr = 'R12_CalibC_B_01_combined_segments.mat';
+exp_var.trainStr = 'R12_CalibC_B_01_combined_segments.mat';
 
 [ errors, calType ] = exp_wrap( exp_var );
 
